7cebd0884fd03146c788c68654a02d7bea64acb2
ajoossharma <165235263+ajoossharma@users.noreply.github.com>
2025-02-13 09:08:21 +0530

    added basic UI components and installed Clerk

---
+++ components/header.jsx
@@ -0,0 +1,13 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+const Header = () => {
+    return <div>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+        <SignedIn>
+            <UserButton />
+        </SignedIn></div>;
+};
+
+export default Header;
