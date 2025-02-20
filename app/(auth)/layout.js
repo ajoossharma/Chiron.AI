2c7b509c9f2e32d76c402bdc11ba83d00ce34621
ajoossharma <165235263+ajoossharma@users.noreply.github.com>
2025-02-20 14:53:05 +0530

    minor changes

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,7 +1,5 @@
-import React from 'react'
-
-const Authlayout = ({children}) => {
-  return <div className="flex justify-center pt-40">{children}</div>
+const AuthLayout = ({ children }) => {
+  return <div className="flex justify-center pt-40">{children}</div>;
 };
 
-export default Authlayout
+export default AuthLayout;
--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,7 +1,5 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
-const Page = () => {
-  return <SignIn />
+export default function Page() {
+  return <SignIn />;
 }
-
-export default Page
